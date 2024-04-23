5fdd1a256803e56c5bb2aba807be2c160b294c1f
Oliver Whitehead <oliver@synx.games>
2024-04-24 00:13:32 +0100

    fix scrippies

--- rebuild_rest.sh
+++ rebuild_rest.sh
@@ -1,7 +1,7 @@
 echo status: Rebuilding Docker Containers
 sleep 1
 echo status: shutting down docker
-docker compose down cmp2808-rest-api
+docker stop cmp2808-ao1-cmp2808-rest-api-1
 sleep 1
 echo status: rebuild rest-api
 docker compose build --no-cache cmp2808-rest-api
--- rebuild_sql.sh
+++ rebuild_sql.sh
@@ -1,7 +1,7 @@
 echo status: Rebuilding Docker Containers
 sleep 1
 echo status: shutting down docker
-docker compose down cmp-2808-mysql
+docker stop cmp2808-ao1-cmp-2808-mysql-1
 sleep 1
 echo status: rebuild mysql
 docker compose build --no-cache cmp-2808-mysql
